2c7863a4497b187335f3bbe9961189a27986b6fa
huihui <503044120@qq.com>
2018-03-22 20:48:50 +0800

    sync"

---
+++ HelloKotlin/app/src/main/java/org/huihui/hellokotlin/Hello.java
@@ -0,0 +1,12 @@
+package org.huihui.hellokotlin;
+
+/**
+ * Created by huihui on 2018/3/22.
+ */
+
+public class Hello {
+    public static String Hello() {
+
+        return "nihao";
+    }
+}
---
+++ HelloKotlin/app/src/main/java/org/huihui/hellokotlin/MainActivity.kt
@@ -0,0 +1,28 @@
+package org.huihui.hellokotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        hello()
+        var a = "1122"
+        var b = 2
+        findViewById<TextView>(R.id.tv)
+    }
+
+    fun hello() {
+        var a = "aaa"
+        var b = "bbb"
+        var c ="""$b
+            $a""".trimIndent()
+        Toast.makeText(this, c, Toast.LENGTH_SHORT).show()
+//        Toast.makeText(this, Hello.Hello(), Toast.LENGTH_SHORT).show()
+
+    }
+}
