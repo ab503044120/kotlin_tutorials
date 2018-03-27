f2fd4bf35b5015aff777955a6d6704617ee55805
huihui <503044120|@qq.com>
2018-03-27 20:33:59 +0800

    sync

--- HelloKotlin/app/src/main/java/org/huihui/hellokotlin/hello2/HomeActivity.kt
+++ HelloKotlin/app/src/main/java/org/huihui/hellokotlin/hello2/HomeActivity.kt
@@ -2,6 +2,7 @@ package org.huihui.hellokotlin.hello2
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import org.huihui.hellokotlin.AbsObj
 
 class HomeActivity(str: String) : AppCompatActivity() {
     init {
@@ -10,5 +11,10 @@ class HomeActivity(str: String) : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        object :AbsObj(){
+            override fun absclass() {
+            }
+
+        }
     }
 }
